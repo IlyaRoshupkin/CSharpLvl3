8fc05a4a46fdc2dafc13a77d447e1019d3a4cb2d
IlyaRoshupkin <roshupkin.ilya@gmail.com>
2020-10-12 18:46:14 +0300

    Homework6

---
+++ MailSender/Homework6/hw6task1/Program.cs
@@ -0,0 +1,64 @@
+﻿//1.Даны 2 двумерных матрицы.
+//Размерность 100х100 каждая. Напишите приложение, 
+//производящее параллельное умножение матриц.
+//Матрицы заполняются случайными целыми числами от 0 до10.
+//Рощупкин
+using System;
+using System.Threading.Tasks;
+
+namespace hw6task1
+{
+    class Program
+    {
+        static Random rnd;
+        private static int[,] GetMatrix(int n)
+        {
+            rnd = new Random();
+            int[,] matrix = new int[n, n];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    matrix[i, j] = rnd.Next(1, 10);
+            Console.WriteLine("Matrix1 had been created");
+
+            return matrix;
+        }
+        // определение асинхронного метода
+        static async Task<int[,]>  MultiplyTwoMatrix()
+        {
+            int[,] m1 = Task.Run(() => GetMatrix(100)).Result;
+            int[,] m2 = Task.Run(() => GetMatrix(100)).Result;
+            await Task.WhenAll();
+            return await Task.Run(() => MultiplyMatrix(m1, m2));
+        }
+
+        private static int[,] MultiplyMatrix(int[,] m1, int[,] m2)
+        {
+            int[,] result = new int[m1.GetLength(0), m2.GetLength(1)];
+            Parallel.For(0, m1.GetLength(0), i =>
+            {
+                for (int j = 0; j < m2.GetLength(1); j++)
+                    for (int k = 0; k < m2.GetLength(0); k++)
+                        result[i, j] += m1[i, k] * m2[k, j];
+            });
+            Console.WriteLine("Multiplication had been completed");
+            return result;
+        }
+        public static void Print(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    Console.Write("{0} ", matrix[i, j]);
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[,] result =MultiplyTwoMatrix().Result;
+            Print(result);
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ MailSender/Homework6/hw6task2v2/Program.cs
@@ -0,0 +1,82 @@
+﻿
+//2. * В некой директории лежат файлы. По структуре они содержат 3 числа, 
+//разделенные пробелами. Первое число — целое, обозначает действие, 1 — умножение 
+//и 2 — деление, остальные два — числа с плавающей точкой. 
+//Написать многопоточное приложение, выполняющее вышеуказанные действия 
+//над числами и сохраняющее результат в файл result.dat. 
+//Количество файлов в директории заведомо много.
+//Рощупкин
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace hw6task2v2
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            StreamWriter streamWriter;
+            string FileName = "expressions.txt";
+            try
+            {
+                var results = await CalculateAsync(FileName).ConfigureAwait(true);
+                streamWriter = new StreamWriter("result.txt");
+                PrintResultAsync("result.dat", results);
+                Console.WriteLine("The calculations have been completed successfully");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        private static async void PrintResultAsync(
+            string FileName, Dictionary<int, double> results)
+        {
+            using (var writer = new StreamWriter(FileName))
+            {
+                foreach (var expression in results.Keys)
+                    await writer.WriteLineAsync(
+                        results[expression].ToString()).ConfigureAwait(false);
+            }
+        }
+
+        private static async Task<Dictionary<int, double>> CalculateAsync(string FileName)
+        {
+            var resultDict = new Dictionary<int, double>();
+            int expression = 1;
+            using (var reader = new StreamReader(FileName))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = await reader.ReadLineAsync().ConfigureAwait(false);
+                    var numbers = line.Split(' ');
+                    await Task.Delay(1);
+
+                    foreach (var number in numbers)
+                    {
+                        switch (int.Parse(numbers[0]))
+                        {
+                            case 1:
+                                resultDict[expression] =
+                                    Double.Parse(numbers[1]) * Double.Parse(numbers[2]);
+                                break;
+                            case 2:
+                                resultDict[expression] =
+                                    Double.Parse(numbers[1]) / Double.Parse(numbers[2]);
+                                break;
+                            default:
+                                resultDict[expression] = Double.NaN;
+                                break;
+                        }
+                        expression++;
+                    }
+                }
+            }
+            return resultDict;
+        }
+    }
+}
